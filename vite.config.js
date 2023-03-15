0fa954c649ef7e1b908dc66f50cd500a47d63522
huoyijie <yijie.huo@foxmail.com>
2023-03-15 13:20:33 +0800

    init

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { fileURLToPath, URL } from 'node:url';
+
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig((/*{ command }*/) => {
+    return {
+        plugins: [vue()],
+        // base: command === 'serve' ? '' : '/goal-vue/',
+        base: '',
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url))
+            }
+        }
+    };
+});

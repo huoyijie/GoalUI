682c9120455637887170cc32799d7bbf7a28b064
huoyijie <yijie.huo@foxmail.com>
2023-03-29 09:31:51 +0800

    group.json refactor

--- src/locale/messages/zh_CN/index.js
+++ src/locale/messages/zh_CN/index.js
@@ -1,3 +1,5 @@
+import group from './group.json';
+
 export default {
     signin: {
         formLabel: '欢迎登录',
@@ -86,40 +88,5 @@ export default {
         post: '新增',
         delete: '删除'
     },
-    group: {
-        admin: {
-            label: '通用管理',
-            operationlog: {
-                label: '操作日志',
-                User: '用户名',
-                Date: '时间',
-                IP: 'IP',
-                Action: '动作',
-                ObjectID: '目标 ID'
-            }
-        },
-        auth: {
-            label: '认证授权',
-            role: {
-                label: '角色',
-                ID: 'ID',
-                Name: '名称'
-            },
-            session: {
-                label: '会话',
-                Key: '会话',
-                User: '用户',
-                ExpireDate: '过期时间'
-            },
-            user: {
-                label: '用户',
-                ID: 'ID',
-                Username: '用户名',
-                Email: '邮箱地址',
-                Password: '密码',
-                IsSuperuser: '超级管理员',
-                IsActive: '有效'
-            }
-        }
-    }
+    group
 };

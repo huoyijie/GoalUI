358576a268ec8a76c0121d9254a9eae0b70fdb9a
huoyijie <yijie.huo@foxmail.com>
2023-04-15 19:10:02 +0800

    refactor mv some config to .env file

--- src/settings/index.js
+++ src/settings/index.js
@@ -1,16 +1,13 @@
-import config from './app.config.json';
 import menu from './app.menu.json';
 import en from './locale.en.json';
 import zh_CN from './locale.zh_CN.json';
 
-export const appConfig = config;
-
 export function getURL(path) {
-    return appConfig.serverContextPath + path;
+    return import.meta.env.VITE_SERVER_CONTEXT_PATH + path;
 }
 
 export function getMediaURL(path) {
-    return appConfig.serverMediaPath + path;
+    return import.meta.env.VITE_SERVER_MEDIA_PATH + path;
 }
 
 export const menuConfig = {

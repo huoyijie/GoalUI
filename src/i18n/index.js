4cc5bf942160bb716c08b4ae0e1a34c9dcaf8999
huoyijie <yijie.huo@foxmail.com>
2023-04-16 13:58:43 +0800

    refactor primevue i18n config

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,13 +1,8 @@
 import { createI18n } from 'vue-i18n';
-import messagesEn from './locale/messages/en';
-import messagesZh_CN from './locale/messages/zh_CN';
-import primeEn from './locale/prime/en';
-import primeZh_CN from './locale/prime/zh_CN';
-
-const messages = {
-    en: primeEn,
-    zh_CN: primeZh_CN
-};
+import messagesEn from './locales/messages/en';
+import messagesZh_CN from './locales/messages/zh_CN';
+import primeEn from './locales/primevue/en.json';
+import primeZh_CN from './locales/primevue/zh_CN.json';
 
 const lang = localStorage.getItem('lang') || import.meta.env.VITE_DEFAULT_LANG || 'en';
 
@@ -25,10 +20,16 @@ i18n.global.langs = [
     { name: '简体中文', value: 'zh_CN' }
 ];
 i18n.global.primevue = {
-    locale: messages[lang]
+    messages: {
+        en: primeEn,
+        zh_CN: primeZh_CN
+    }
 };
+const { primevue } = i18n.global;
+primevue.locale = primevue.messages[lang];
 
 i18n.global.setLang = (primevue, lang) => {
+    const { messages } = i18n.global.primevue;
     primevue.config.locale = messages[lang];
     i18n.global.locale.value = lang;
     localStorage.setItem('lang', lang);
